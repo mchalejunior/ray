ea8f79223bccee68db48756c47308b4f5c539320
Cathal McHale <cathal.mchale@pivotts.com>
2019-07-03 12:40:21 +0100

    will not discover new test, no matter what! will push to server to see if build box does recognize.

---
+++ test/Ray.Domain.Test/Tuples/BasicOperationsTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace Ray.Domain.Test.Tuples
+{
+    [FeatureFile("./features/tuples/BasicOperations.feature")]
+    public sealed class BasicOperationsTests : Feature
+    {
+        private Vector4 _firstTuple = new Vector4(),
+            _secondTuple = new Vector4();
+
+        [Given(@"a1 = tuple (\d) (-\d) (\d) (\d)")]
+        public void One(float x, float y, float z, float w)
+        {
+            _firstTuple.X = x;
+            _firstTuple.Y = y;
+            _firstTuple.Z = z;
+            _firstTuple.W = w;
+        }
+
+        [And(@"a2 = tuple (-\d) (\d) (\d) (\d)")]
+        public void Two(float x, float y, float z, float w)
+        {
+            _secondTuple.X = x;
+            _secondTuple.Y = y;
+            _secondTuple.Z = z;
+            _secondTuple.W = w;
+        }
+
+        [Then(@"a1 plus a2 = tuple (\d) (\d) (\d) (\d)")]
+        public void Three(float x, float y, float z, float w)
+        {
+            var expectedResult = new Vector4(x, y, z, w);
+            var actualResult = Vector4.Add(_firstTuple, _secondTuple);
+            Assert.True(expectedResult.Equals(actualResult));
+        }
+
+    }
+}
