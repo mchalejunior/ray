fbd4337b196f616d001c1c541cae14bc11ec0c3c
Cathal McHale <cathal.mchale@pivotts.com>
2019-07-03 16:26:18 +0100

    Finally working. Logged issue:
    https://github.com/ttutisani/Xunit.Gherkin.Quick/issues/82

--- test/Ray.Domain.Test/Tuples/BasicMathOpsTests.cs
+++ test/Ray.Domain.Test/Tuples/BasicMathOpsTests.cs
@@ -11,7 +11,7 @@ namespace Ray.Domain.Test.Tuples
             _secondTuple = new Vector4();
 
         [Given(@"a1 = tuple (\d) (-\d) (\d) (\d)")]
-        public void One(float x, float y, float z, float w)
+        public void InitializationValues_SetOnFirstTuple(float x, float y, float z, float w)
         {
             _firstTuple.X = x;
             _firstTuple.Y = y;
@@ -20,7 +20,7 @@ namespace Ray.Domain.Test.Tuples
         }
 
         [And(@"a2 = tuple (-\d) (\d) (\d) (\d)")]
-        public void Two(float x, float y, float z, float w)
+        public void InitializationValues_SetOnSecondTuple(float x, float y, float z, float w)
         {
             _secondTuple.X = x;
             _secondTuple.Y = y;
@@ -29,12 +29,11 @@ namespace Ray.Domain.Test.Tuples
         }
 
         [Then(@"a1 plus a2 = tuple (\d) (\d) (\d) (\d)")]
-        public void Three(float x, float y, float z, float w)
+        public void GivenExpectedAnswer_PerformAddition_VerifyResult(float x, float y, float z, float w)
         {
             var expectedResult = new Vector4(x, y, z, w);
             var actualResult = Vector4.Add(_firstTuple, _secondTuple);
             Assert.True(expectedResult.Equals(actualResult));
         }
-
     }
 }
--- test/Ray.Domain.Test/features/tuples/BasicMathOps.feature
+++ test/Ray.Domain.Test/features/tuples/BasicMathOps.feature
@@ -2,8 +2,8 @@ Feature: BasicTuplesMathFeature
 	In order to move around 3D space
 	As a Ray Tracer
 	I want the ability to perform basic math on points and vectors
-
-Scenario​: Adding two tuples
+ 
+Scenario: Adding two tuples
 	Given a1 = tuple 3 -2 5 1
-	​​And​ a2 = tuple -2 3 1 0
-	​​Then​ a1 plus a2 = tuple 1 1 6 1
+	And a2 = tuple -2 3 1 0
+	Then a1 plus a2 = tuple 1 1 6 1
