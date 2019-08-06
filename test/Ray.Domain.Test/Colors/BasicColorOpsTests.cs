1dc63659a383c1555a159150d1ebaeb27501e2a3
Cathal McHale <cathal.mchale@pivotts.com>
2019-08-06 14:08:13 +0100

    upgraded to .net core 3.0 preview.

---
+++ test/Ray.Domain.Test/Colors/BasicColorOpsTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+//using System.Windows.Media;
+using System.Text;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace Ray.Domain.Test.Colors
+{
+    [FeatureFile("./features/colors/BasicColorOps.feature")]
+    public sealed class BasicColorOpsTests : Feature
+    {
+        private Color _firstColor, _secondColor;
+
+        [Given(@"c1 equals color (\d+) (\d+) (\d+)")]
+        public void InitializationValues_SetOnTupleInstance(int r, int g, int b)
+        {
+            _firstColor = Color.FromArgb(r, g, b);
+        }
+
+        [And(@"c2 equals color (\d+) (\d+) (\d+)")]
+        public void InitializationValues_SetOtherTupleInstance(int r, int g, int b)
+        {
+            _secondColor = Color.FromArgb(r, g, b);
+        }
+
+        [Then(@"c1.red equals (\d+)")]
+        public void ReadRed_VerifyValue(int r)
+        {
+            Assert.Equal(r, _firstColor.R);
+        }
+
+        [And(@"c1.green equals (\d+)")]
+        public void ReadGreen_VerifyValue(int g)
+        {
+            Assert.Equal(g, _firstColor.G);
+        }
+
+        [And(@"c1.blue equals (\d+)")]
+        public void ReadBlue_VerifyValue(int b)
+        {
+            Assert.Equal(b, _firstColor.B);
+        }
+
+        [Then(@"c1 plus c2 equals color (\d+) (\d+) (\d+)")]
+        public void GivenExpectedAnswer_PerformSubtraction_VerifyResult(int r, int g, int b)
+        {
+            var expectedResult = Color.FromArgb(r, g, b);
+            //var actualResult = _firstColor + _secondColor;
+        }
+    }
+}
---
+++ test/Ray.Domain.Test/features/colors/BasicColorOps.feature
@@ -0,0 +1,18 @@
+﻿Feature: BasicColorOpsFeature
+	In order to draw realistic 3D scenes
+	As a Ray Tracer
+	I want the ability to draw pixels in different colors
+ 
+Scenario: Colors are (Red, Green, Blue) tuples
+	Given c1 equals color 0 74 255
+	Then c1.red equals 0
+	And c1.green equals 74
+	And c1.blue equals 255
+
+
+Scenario: Adding colors
+	Given c1 equals color 0 74 255
+	And c2 equals color 125 55 20
+	Then c1 plus c2 equals color 125 129 255
+
+
