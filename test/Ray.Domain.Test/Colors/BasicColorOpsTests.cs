4b88f6b558682a1ad56573a03eb916f2d39d982c
Cathal McHale <cathal.mchale@pivotts.com>
2019-08-06 17:31:33 +0100

    Using WPF Color - close to Ray Tracer Challenge expectations.

--- test/Ray.Domain.Test/Colors/BasicColorOpsTests.cs
+++ test/Ray.Domain.Test/Colors/BasicColorOpsTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-//using System.Windows.Media;
-using System.Text;
+﻿using System.Windows.Media;
 using Xunit;
 using Xunit.Gherkin.Quick;
 
@@ -13,41 +9,57 @@ namespace Ray.Domain.Test.Colors
     {
         private Color _firstColor, _secondColor;
 
-        [Given(@"c1 equals color (\d+) (\d+) (\d+)")]
-        public void InitializationValues_SetOnTupleInstance(int r, int g, int b)
+        [Given(@"c1 equals color (-?\d+\.\d+) (-?\d+\.\d+) (-?\d+\.\d+)")]
+        public void InitializationValues_SetOnTupleInstance(float r, float g, float b)
         {
-            _firstColor = Color.FromArgb(r, g, b);
+            _firstColor = new Color
+            {
+                ScR = r,
+                ScG = g,
+                ScB = b
+            };
         }
 
-        [And(@"c2 equals color (\d+) (\d+) (\d+)")]
-        public void InitializationValues_SetOtherTupleInstance(int r, int g, int b)
+        [And(@"c2 equals color (-?\d+\.\d+) (-?\d+\.\d+) (-?\d+\.\d+)")]
+        public void InitializationValues_SetOtherTupleInstance(float r, float g, float b)
         {
-            _secondColor = Color.FromArgb(r, g, b);
+            _secondColor = new Color
+            {
+                ScR = r,
+                ScG = g,
+                ScB = b
+            };
         }
 
-        [Then(@"c1.red equals (\d+)")]
-        public void ReadRed_VerifyValue(int r)
+        [Then(@"c1.red equals (-?\d+\.\d+)")]
+        public void ReadRed_VerifyValue(float r)
         {
-            Assert.Equal(r, _firstColor.R);
+            Assert.Equal(r, _firstColor.ScR);
         }
 
-        [And(@"c1.green equals (\d+)")]
-        public void ReadGreen_VerifyValue(int g)
+        [And(@"c1.green equals (-?\d+\.\d+)")]
+        public void ReadGreen_VerifyValue(float g)
         {
-            Assert.Equal(g, _firstColor.G);
+            Assert.Equal(g, _firstColor.ScG);
         }
 
-        [And(@"c1.blue equals (\d+)")]
-        public void ReadBlue_VerifyValue(int b)
+        [And(@"c1.blue equals (-?\d+\.\d+)")]
+        public void ReadBlue_VerifyValue(float b)
         {
-            Assert.Equal(b, _firstColor.B);
+            Assert.Equal(b, _firstColor.ScB);
         }
 
-        [Then(@"c1 plus c2 equals color (\d+) (\d+) (\d+)")]
-        public void GivenExpectedAnswer_PerformSubtraction_VerifyResult(int r, int g, int b)
+        [Then(@"c1 plus c2 equals color (-?\d+\.\d+) (-?\d+\.\d+) (-?\d+\.\d+)")]
+        public void GivenExpectedAnswer_PerformAddition_VerifyResult(float r, float g, float b)
         {
-            var expectedResult = Color.FromArgb(r, g, b);
-            //var actualResult = _firstColor + _secondColor;
+            var expectedResult = new Color
+            {
+                ScR = r,
+                ScG = g,
+                ScB = b
+            };
+            var actualResult = _firstColor + _secondColor;
+            Assert.True(Color.AreClose(expectedResult, actualResult));
         }
     }
 }
--- test/Ray.Domain.Test/features/colors/BasicColorOps.feature
+++ test/Ray.Domain.Test/features/colors/BasicColorOps.feature
@@ -4,15 +4,15 @@
 	I want the ability to draw pixels in different colors
  
 Scenario: Colors are (Red, Green, Blue) tuples
-	Given c1 equals color 0 74 255
-	Then c1.red equals 0
-	And c1.green equals 74
-	And c1.blue equals 255
+	Given c1 equals color 0.5 0.4 0.9
+	Then c1.red equals 0.5
+	And c1.green equals 0.4
+	And c1.blue equals 0.9
 
 
 Scenario: Adding colors
-	Given c1 equals color 0 74 255
-	And c2 equals color 125 55 20
-	Then c1 plus c2 equals color 125 129 255
+	Given c1 equals color 0.0 0.5 0.8
+	And c2 equals color 0.9 0.45 0.15
+	Then c1 plus c2 equals color 0.9 0.95 0.95
 
 
